722108c21590acae96860a6dc1f6531a816e51b8
folone <folone@gmail.com>
2013-03-14 10:57:58 +0100

    scala 2.9.1 → 2.9.2, scalaz 6.0.4 → 7.0.0-M8
    Signed-off-by: folone <folone@gmail.com>

--- common/src/main/scala/com/bwater/notebook/As.scala
+++ common/src/main/scala/com/bwater/notebook/As.scala
@@ -9,9 +9,9 @@ package com.bwater.notebook
 
 import scalaz._, Scalaz._
 
-case class As[P[_], A](value: P[A]) extends NewType[P[A]]
+case class As[P[_], A](self: P[A]) extends syntax.Ops[P[A]]
 
 object As {
-  implicit def pureToAs[P[_], A](a: A)(implicit P: Pure[P]): As[P, A] = As[P, A](P.pure(a))
+  implicit def pureToAs[P[_], A](a: A)(implicit P: Applicative[P]): As[P, A] = As[P, A](P.pure(a))
   implicit def identityToAs[P[_], A](a: P[A]): As[P, A] = As[P, A](a)
 }
