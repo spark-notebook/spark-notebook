146062e10c29f779f95471352aa09b94d90cfd51
Andy Petrella <andy.petrella@gmail.com>
2014-10-06 01:35:55 +0200

    Fixes and new stuffs * Passes arguments (json object) to functions called withing Playgroung * RickShaw is buggy for movable window, but workaround applied and issue created -- hence we can see *new* data arriving in a window filled by Scala futures. * added an argument to `run` : `no_browser` to not try to open the browser (which can be quickly annoying while testing and when deployed on servers) * fixes the duplicated piublishing event in playground.coffee for dataO (removed application, that was publishing it)

--- common/src/main/scala/com/bwater/notebook/d3/struct.scala
+++ common/src/main/scala/com/bwater/notebook/d3/struct.scala
@@ -4,13 +4,14 @@ import net.liftweb.json.JsonAST._
 import net.liftweb.json.JsonDSL._
 import com.bwater.notebook._, JsonCodec._
 
-case class Series(color:String, data:Seq[Map[String, Double]])
+case class Series(name:String, color:String, data:Seq[Map[String, Double]])
 
 object Series {
   implicit val seriesCodec = new Codec[JValue, Series] {
     def encode(vs: JValue):Series = ???
     def decode(ts: Series): JValue = 
       JObject(List(
+        JField("name", JString(ts.name)), 
         JField("color", JString(ts.color)), 
         JField("data", implicitly[Codec[JValue, Seq[Map[String, Double]]]].decode(ts.data))
       ))
