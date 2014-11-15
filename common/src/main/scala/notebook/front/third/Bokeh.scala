00c9370c2a7081c418fb349a193e4bbac57228ec
Gerrit <gerrit.gruben@gmail.com>
2014-11-15 17:11:33 +0100

    Refactored Code. Added functionality to make simple function graph plots for real-valued functions

--- common/src/main/scala/notebook/front/third/Bokeh.scala
+++ common/src/main/scala/notebook/front/third/Bokeh.scala
@@ -17,12 +17,47 @@ class Bokeh(plotContext : PlotContext) extends Widget {
 }
 
 object Bokeh {
+  /**
+   * Renders the plots onto the display
+   */
   def plot(plots : List[Plot]) = new Bokeh(new PlotContext().children(plots))
 
 
+  /**
+   * Plots function graph
+   * @param xs The parameter values.
+   * @param f The function to the computed (real-valued right now).
+   */
+  def functionGraph(xs: Seq[Double], f: Double => Double) = {
+    val ys = xs map f
+
+    val source = new ColumnDataSource()
+      .addColumn('x, xs)
+      .addColumn('y, ys)
+    val xdr = new DataRange1d().sources(source.columns('x) :: Nil)
+    val ydr = new DataRange1d().sources(source.columns('y) :: Nil)
+    val plot = new Plot().x_range(xdr).y_range(ydr)
+
+    val glyphs = new Glyph()
+      .data_source(source)
+      .glyph(new Circle().x('x).y('y).radius(0.01))
+
+    val xaxis = new LinearAxis().plot(plot).location(Location.Below)
+    val yaxis = new LinearAxis().plot(plot).location(Location.Left)
+
+    plot.below <<= (xaxis :: _)
+    plot.left <<= (yaxis :: _)
+
+    plot.renderers := List(xaxis, yaxis, glyphs)
+    plot
+  }
+
+  def plotFunctionGraph(xs: Seq[Double], f: Double => Double) = plot(functionGraph(xs, f) :: Nil)
+
+  // Scatter plot constructor
   case class ScatterPoint(x: Double, y: Double)
 
-  def scatterPlot(points : Seq[ScatterPoint]) = {
+  def scatter(points : Seq[ScatterPoint]) = {
     def buildPlotBase(source: DataSource) = {
       val xdr = new DataRange1d().sources(source.columns('x) :: Nil)
       val ydr = new DataRange1d().sources(source.columns('y) :: Nil)
@@ -47,7 +82,9 @@ object Bokeh {
     plot.left <<= (yaxis :: _)
 
     plot.renderers := List(xaxis, yaxis, glyphs)
-    Bokeh.plot(plot :: Nil)
+    plot
   }
+
+  def scatterPlot(points : Seq[ScatterPoint]) = plot(scatter(points) :: Nil)
 }
 
