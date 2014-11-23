010f663fcd482105c34a26127fd434c86cb0aba0
Andy Petrella <andy.petrella@gmail.com>
2014-11-23 09:08:39 -0800

    extract dynamic form handling out of the Spark configuratorµ

---
+++ common/src/main/scala/notebook/front/widgets/Form.scala
@@ -0,0 +1,76 @@
+package notebook.front.widgets
+
+import org.json4s.JsonAST._
+import org.json4s.JsonDSL._
+
+import notebook.Codec
+import notebook.JsonCodec._
+import notebook.front.SingleConnectedWidget
+
+trait Form[D] extends SingleConnectedWidget[Map[String, String]] {
+  def initData:D
+  def paramsCodec:Codec[D, Map[String, String]]
+  def update:D => D
+
+  private[widgets] var data:D = initData
+  implicit val codec:Codec[JValue, Map[String, String]] = tMap[String]
+
+  currentData.observable.inner.subscribe{ m =>
+    data = paramsCodec.decode(m)
+    update(data) // because the whole reactive `workflow` is not there... yet?
+  }
+
+  lazy val toHtml = <div>{
+      scopedScript(
+        """require( ['observable', 'knockout', 'jquery'],
+                    function (Observable, ko, $) {
+                      var value = Observable.makeObservable(valueId);
+                      var publishFormData = function(form) {
+                        var r = $(form).serializeArray();
+                        var result = {};
+                        r.forEach(function(o) {
+                          result[o.name] = o.value;
+                        });
+                        value(result);
+                      };
+                      var addEntry = function(form) {
+                        var entry = $(form).serializeArray()[0];
+                        $("#update-spark-form").find("button")
+                                               .before("<div class='form-group'><label for-name='"+entry.value+"'>"+entry.value+"</label><input class='form-control' name='"+entry.value+"' value=''/></div>")
+                      };
+                      ko.applyBindings({
+                        formShown:        ko.observable(false),
+                        value:            value,
+                        publishFormData:  publishFormData,
+                        addEntry:         addEntry
+                      }, this);
+                    }
+                  )""",
+        ("valueId" -> dataConnection.id)
+      )
+    }
+    <span class="help-block">
+      <input type="checkbox" data-bind="checked: formShown" />Update Spark configuration
+    </span>
+    <div data-bind="if: formShown">
+      <form role="form" id="update-spark-form" data-bind="submit: publishFormData">
+        {
+          paramsCodec.encode(data).map { case (k, v) =>
+            <div class="form-group">
+              <label for-name={k}>{k}</label>
+              <input name={k} value={v} class="form-control"/>
+            </div>
+          }
+        }
+        <button type="submit" class="btn btn-default">Change</button>
+      </form>
+      <form data-bind="submit: addEntry" role="form">
+        <div class="form-group">
+          <label for-name="add-entry">Add entry</label>
+          <input name="add-entry" class="form-control" type="text" />
+        </div>
+        <button type="submit" class="btn btn-default">Add</button>
+      </form>
+    </div>
+  </div>
+}
