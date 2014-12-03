b2e065d4bdd7a96c8cff10309cb38d93fa64c05f
Andy Petrella <andy.petrella@gmail.com>
2014-12-03 23:38:27 +0100

    added :dp, remote-repo and local-repo contexts
    :dp
    ---
    We can declare that can declare deps "à la" sbt (but only mvn ones still →  #33 ):
    * include with a '+' or nothing  and exclude deps"
    * exclude transitively by starting with a '-'
    
    Jars will be added download to the local repo from the remote repos.
    Then the list of paths will be added to spark.jars
    Finally, the spark context will be restarted
    
    :local-repo
    Update the local repo where jars will be downloaded
    
    :remote-repo
    Add a remote repo from where deps will be downloaded

--- common/src/main/scala/notebook/util/Deps.scala
+++ common/src/main/scala/notebook/util/Deps.scala
@@ -1,12 +1,12 @@
 package notebook.util
 
-import org.sonatype.aether.repository.RemoteRepository
+import scala.collection.JavaConversions._
 import com.jcabi.aether.Aether
 import java.util.Arrays
 import org.apache.maven.project.MavenProject
+import org.sonatype.aether.repository.RemoteRepository
 import org.sonatype.aether.artifact.Artifact
 import org.sonatype.aether.util.artifact.DefaultArtifact
-import scala.collection.JavaConversions._
 import org.sonatype.aether.graph._
 import org.sonatype.aether.util.filter.ExclusionsDependencyFilter
 
@@ -20,31 +20,70 @@ object Repos extends java.io.Serializable {
   def apply(id:String, name:String, url:String) = new RemoteRepository(id, name, url)
 }
 
+case class ArtifactMD(group:String, artifact:String, version:String)
+case class ArtifactSelector(group:Option[String]=None, artifact:Option[String]=None, version:Option[String]=None)
+object ArtifactSelector {
+  def apply(group:String, artifact:String, version:String):ArtifactSelector =
+    ArtifactSelector(Some(group), Some(artifact), Some(version))
+  def group(group:String) =
+    ArtifactSelector(group=Some(group))
+  def artifact(group:String, artifact:String) =
+    ArtifactSelector(group=Some(group), artifact=Some(artifact))
+}
+
 object Deps extends java.io.Serializable {
-  type ArtifactMD = (String, String, String)
   type ArtifactPredicate = PartialFunction[(ArtifactMD, Set[ArtifactMD]), Boolean]
 
-  def matchAMD(group:Option[String], artifact:Option[String], version:Option[String], a:ArtifactMD) =
-      group.getOrElse(a._1) == a._1 && artifact.getOrElse(a._2) == a._2 && version.getOrElse(a._3) == a._3
-
-  def transitiveExclude(group:Option[String]=None, artifact:Option[String]=None, version:Option[String]=None):ArtifactPredicate = {
-    case (a, _) if matchAMD(group, artifact, version, a) => true
-    case (_, xs) => {
-        val p:PartialFunction[ArtifactMD,Boolean] = { case a if matchAMD(group, artifact, version, a) => true }
-        xs exists (x => p.isDefinedAt(x) && p(x))
+  def parseInclude(s:String):Option[ArtifactMD] = {
+    s.headOption.filter(_ != '-').map(_ => s.dropWhile(_=='+').trim).flatMap { line =>
+      line.split("%").toList match {
+        case List(g, a, v) =>
+          Some(ArtifactMD(g.trim, a.trim, v.trim))
+        case _             =>
+          None
       }
+    }
   }
 
-  def resolveAndAddToJars ( group:String, artifact:String, version:String,
-                            exclusions:Set[ArtifactPredicate]=Set.empty
-                          )(implicit remotes:List[RemoteRepository], repo:java.io.File) = {
+  def parsePartialExclude = (s:String) => s.trim match {
+    case "_" => None
+    case ""  => None
+    case x   => Some(x)
+  }
+  def parseExclude(s:String):Option[ArtifactSelector] = {
+    s.headOption.filter(_ == '-').map(_ => s.dropWhile(_=='-').trim).flatMap { line =>
+      line.split("%").toList match {
+        case List(g, a, v) =>
+          Some(ArtifactSelector(parsePartialExclude(g), parsePartialExclude(a), parsePartialExclude(v)))
+        case _             =>
+          None
+      }
+    }
+  }
+
+  def matchAMD(selector:ArtifactSelector, a:ArtifactMD) =
+      selector.group.getOrElse(a.group) == a.group &&
+      selector.artifact.getOrElse(a.artifact) == a.artifact &&
+      selector.version.getOrElse(a.version) == a.version
+
+  def transitiveExclude(selector:ArtifactSelector):ArtifactPredicate = {
+    case (a, _) if matchAMD(selector, a) => true
+    case (_, xs) => {
+      val p:PartialFunction[ArtifactMD,Boolean] = { case a if matchAMD(selector, a) => true }
+      xs exists (x => p.isDefinedAt(x) && p(x))
+    }
+  }
+
+
+  def resolve (include:ArtifactMD, exclusions:Set[ArtifactPredicate]=Set.empty)
+              (implicit remotes:List[RemoteRepository], repo:java.io.File) = {
     val exc = new DependencyFilter {
       def accept(node:DependencyNode, parents:java.util.List[DependencyNode] ):Boolean = {
         val ex = exclusions exists { case f =>
                   val na = node.getDependency.getArtifact
-                  val a = (na.getGroupId, na.getArtifactId, na.getVersion)
+                  val a = ArtifactMD(na.getGroupId, na.getArtifactId, na.getVersion)
                   val sa = parents.map(n => n.getDependency.getArtifact)
-                                  .map(na => (na.getGroupId, na.getArtifactId, na.getVersion))
+                                  .map(na => ArtifactMD(na.getGroupId, na.getArtifactId, na.getVersion))
                                   .toSet
                   f.isDefinedAt((a, sa)) && f(a, sa)
                 }
@@ -53,12 +92,14 @@ object Deps extends java.io.Serializable {
     }
 
     val deps:Set[Artifact] =  new Aether(remotes, repo).resolve(
-                                new DefaultArtifact(group, artifact, "", "jar", version),
+                                new DefaultArtifact(include.group, include.artifact, "", "jar", include.version),
                                 "runtime",
                                 exc
                               ).toSet
 
     val newJars = deps.map(_.getFile.getPath).toSet.toList
     newJars
+
   }
+
 }
