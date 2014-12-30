36d988190bc5d06a68355144e9ef74981367c441
Andy Petrella <andy.petrella@gmail.com>
2014-12-30 02:47:36 +0100

    fixes #21
    This one support just some rundimentary combinors (the weak way): | and &&.
    Best would be to use `sys.process` directly from a *code cell* for more elaborated shell scripts
    
    This commit cleans a bit the sbt from unused stuffs and fixes scalaVersion to 2.10.4 everywhere.
    
    It also allows for specifying -Dspark.version and -Dhadoop.version in order to build the notebook against specific versions.
    This is only a quick and dirty (and probably ephemeral) support, cross builds would be better.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 
 object Dependencies {
+
   val akkaVersion           = "2.2.3-shaded-protobuf"
 
   val playDep               = "com.typesafe.play"         %%            "play"              %      "2.2.6"        excludeAll(ExclusionRule("com.typesafe.akka"))
@@ -9,26 +10,25 @@ object Dependencies {
   val akka                  = "org.spark-project.akka"    %%         "akka-actor"           %    akkaVersion
   val akkaRemote            = "org.spark-project.akka"    %%         "akka-remote"          %    akkaVersion
   val akkaSlf4j             = "org.spark-project.akka"    %%          "akka-slf4j"          %    akkaVersion
-  val akkaTestkit           = "org.spark-project.akka"    %%         "akka-testkit"         %    akkaVersion    % "test"
 
   val rxScala               = "io.reactivex"              %%           "rxscala"            %      "0.22.0"
 
   val scalaZ                = "org.scalaz"                %%         "scalaz-core"          %      "7.0.6"
 
-  val sparkRepl             = "org.apache.spark"          %%         "spark-repl"           %      "1.1.0"      //% "provided"
-  val sparkSQL              = "org.apache.spark"          %%         "spark-sql"            %      "1.1.0"      //% "provided"
+  val sparkVersion          = sys.props.getOrElse("spark.version", default = "1.1.0")
+  val hadoopVersion         = sys.props.getOrElse("hadoop.version", default = "1.0.4")
+  val sparkRepl             = "org.apache.spark"          %%         "spark-repl"           %    sparkVersion     excludeAll(ExclusionRule("org.apache.hadoop"))
+  val sparkSQL              = "org.apache.spark"          %%         "spark-sql"            %    sparkVersion     excludeAll(ExclusionRule("org.apache.hadoop"))
+  val hadoopClient          = "org.apache.hadoop"         %         "hadoop-client"         %    hadoopVersion
 
   val commonsIO             = "org.apache.commons"        %          "commons-io"           %      "1.3.2"
   val commonsHttp           = "org.apache.httpcomponents" %          "httpclient"           %      "4.3.4"
-  val commonsExec           = "org.apache.commons"        %          "commons-exec"         %       "1.2"
+  val commonsExec           = "org.apache.commons"        %          "commons-exec"         %       "[1.2, 1.2]" force()
 
   val guava                 = "com.google.guava"          %            "guava"              %     "14.0.1"
 
   val slf4jLog4j            = "org.slf4j"                 %         "slf4j-log4j12"         %      "1.7.7"
   val log4j                 = "log4j"                     %             "log4j"             %      "1.2.17"
-
-  val scalaTest             = "org.scalatest"             %%          "scalatest"           %      "2.2.0"      % "test"
-  val scalaMock             = "org.scalamock"             %% "scalamock-scalatest-support"  %     "3.1.RC1"     % "test"
 
   // to download deps at runtime
   val aetherApi             = "org.sonatype.aether"       %          "aether-api"           %     "1.13"
