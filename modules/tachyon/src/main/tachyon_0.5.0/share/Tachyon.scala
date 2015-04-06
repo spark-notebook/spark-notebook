d43b2fc63e0dc937dd1d626fa3986d3a48318b2b
Andy Petrella <andy.petrella@gmail.com>
2015-04-06 19:39:22 +0200

    update doc for tachyon 0.5.0 param

--- modules/tachyon/src/main/tachyon_0.5.0/share/Tachyon.scala
+++ modules/tachyon/src/main/tachyon_0.5.0/share/Tachyon.scala
@@ -9,11 +9,7 @@ object Tachyon {
 
   // From tachyon 0.6.3 tests code : tachyon.client.TachyonFSTest
 
-  //tachyon.worker.hierarchystore.level{x}.dirs.quota The quotas for all storage directories in a storage layer,
-  //  which are also be delimited by comma. x represents the storage layer. Workers use the corresponding quota in
-  //  the configuration for storage directories. If the quota for some storage directories are not set, the last
-  //  quota will be used. There is default quota(128MB) for storage layer with alias MEM, if the quota for any
-  //  other storage layer is not set, the system will report the error and exit the initialization.
+  //tachyon.worker.memory.size: Memory capacity of each worker node (128 MB by default)
   val WorkerCapacityBytes = 1024 * 1024 * 1024
 
   //The minimum number of bytes that will be requested from a client to a worker at a time
