0beef69b45df792392b28395c45258d9bf9109aa
minyk <minykreva@gmail.com>
2015-04-18 16:07:16 +0900

    Second step for adding lighting-viz support.  - Use pym.js to import iframe.

--- modules/common/src/main/scala/notebook/front/third/LightningViz.scala
+++ modules/common/src/main/scala/notebook/front/third/LightningViz.scala
@@ -1,13 +1,32 @@
 package notebook.front.third
 
+import notebook.front.Widget
+import notebook.front.widgets._
 import org.viz.lightning.Visualization
+import play.api.libs.json.Json
 
 import scala.xml.NodeSeq
 
 object LightningViz {
 
-  def show(data: Visualization): NodeSeq = {
-    <iframe src={data.getIframeLink} width="100%" class="lightning" height="600"></iframe>
+  def show(data: Visualization): LightningViz = {
+    new LightningViz(data)
   }
+
+}
+
+class LightningViz(data: Visualization) extends Widget {
+
+  lazy val toHtml = <div id={"pymIframe" + data.vid}>{
+    scopedScript(
+    """
+      |req( ['pym'],
+      |  function (pym) {
+      |   var pymIframe = new pym.Parent('pymIframe'+vid, pymLink, {})
+      |  }
+      |);""".stripMargin,
+      Json.obj("pymLink" -> data.getPymLink, "vid" -> data.vid)
+    )
+  }</div>
 
 }
