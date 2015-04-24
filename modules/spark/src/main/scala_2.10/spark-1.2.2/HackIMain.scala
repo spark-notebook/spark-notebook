1815d730c740c0cff077121c7ddd9cecd9d4b7a8
Andy Petrella <andy.petrella@gmail.com>
2015-04-24 17:17:35 +0200

    1.2.2, 1.3.1 support. Add with.hive sys prop to enable hive support

---
+++ modules/spark/src/main/scala_2.10/spark-1.2.2/HackIMain.scala
@@ -0,0 +1,10 @@
+package scala.tools.nsc
+package interpreter
+
+/**
+ * Subclass to access some hidden things I need and also some custom behavior.
+ */
+class HackIMain(settings: Settings, out: JPrintWriter) extends IMain(settings, out) {
+  def previousRequests = prevRequestList
+  override protected def parentClassLoader = settings.getClass.getClassLoader()
+}
