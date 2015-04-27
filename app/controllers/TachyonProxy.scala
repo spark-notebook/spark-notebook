46af1c5c38309d6a0df3a2a05f0d761775915ac2
Andy Petrella <andy.petrella@gmail.com>
2015-04-27 23:22:45 +0200

    added tachyon proxy endpoint to list the content

--- app/controllers/TachyonProxy.scala
+++ app/controllers/TachyonProxy.scala
@@ -4,10 +4,11 @@ import java.util.UUID
 import java.io.File
 import java.net.URLDecoder
 
-import scala.util.{Try, Success, Failure}
+import scala.collection.JavaConverters._
 import scala.concurrent.{Promise, Future}
 import scala.concurrent.duration._
 import scala.reflect.ClassTag
+import scala.util.{Try, Success, Failure}
 
 import play.api._
 import play.api.mvc._
@@ -23,15 +24,24 @@ import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
 
+import tachyon.client.TachyonFS
+
 import notebook._
 import notebook.server._
 import notebook.kernel.remote._
 import notebook.NBSerializer.Metadata
 
 object TachyonProxy extends Controller {
+  lazy val tachyonUrl = AppUtils.config.tachyonInfo.url
+  lazy val shareDir = AppUtils.config.tachyonInfo.baseDir
+  lazy val tachyonClient = tachyonUrl.map( url => TachyonFS.get(url)).getOrElse(notebook.share.Tachyon.fs)
 
-  def ls(path:String = "/") = Action.async {
+  import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-  }
+
+  def ls(path:String = shareDir) = Action.async { Future {
+    val list = tachyonClient.ls(path, false).asScala
+    Ok(Json.toJson(list))
+  } }
 
 }
