bb12f898f1cfc6582b1e8c3d94c163545f6e9cba
Andy Petrella <andy.petrella@gmail.com>
2015-07-20 18:30:39 +0200

    fixes #187 in the notebook: notebooks/viz/WISP - Quick Start.snb. Add wisp 0.5.0 manually

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -139,7 +139,15 @@ object Dependencies {
 
   // Viz
   val bokeh = "io.continuum.bokeh" %% "bokeh" % "0.2"
-  val wisp = "com.quantifind" %% "wisp" % "0.0.2" excludeAll ExclusionRule("com.google.guava")
+  val wispDepSumac = "com.quantifind" %% "sumac" % "0.3.0"
+  //"com.quantifind" %% "wisp" % "0.0.4" excludeAll(
+  //  ExclusionRule("com.google.guava"),
+  //  ExclusionRule("org.json4s"),
+  //  ExclusionRule("net.databinder", "unfiltered-filter"),
+  //  ExclusionRule("net.databinder", "unfiltered-jetty"),
+  //  ExclusionRule("org.apache.commons", "commons-math3"),
+  //  ExclusionRule("commons-io", "commons-io")
+  //)
   // wisp deps on jackson-module-scala_2.10 v2.4 → guava v15
   // but spark → guava 14.0.1
   val customJacksonScala = Seq(
