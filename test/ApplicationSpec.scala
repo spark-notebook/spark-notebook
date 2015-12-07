4f77c576189724cfd67d32c562250f90f742d2ab
vidmantas zemleris <vidmantas@vinted.com>
2015-12-07 20:29:31 +0200

    Improve the tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -24,7 +24,8 @@ class ApplicationSpec extends Specification {
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Spark Notebook")
+
+      contentAsString(home) must contain ("Spark Notebook</a>")
     }
   }
 }
