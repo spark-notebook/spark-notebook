8dfe981a0d5ce491426e5cfdbbb0a1cdf0a1be20
vidmantas zemleris <vidmantas@vinted.com>
2016-01-05 11:44:52 +0200

    scrollTo a variable definition

--- public/ipython/custom/custom.js
+++ public/ipython/custom/custom.js
@@ -210,6 +210,25 @@ require(["jquery", "jquery.gridster"], function($, gridster) {
 
 
 require(["jquery", "underscore", "base/js/events", "knockout"], function($, _, events, ko) {
+  // http://lions-mark.com/jquery/scrollTo/
+  $.fn.scrollTo = function( target, options, callback ){
+      if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+      var settings = $.extend({
+          scrollTarget  : target,
+          offsetTop     : 50,
+          duration      : 500,
+          easing        : 'swing'
+      }, options);
+      return this.each(function(){
+          var scrollPane = $(this);
+          var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+          var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+          scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+              if (typeof callback == 'function') { callback.call(this); }
+          });
+      });
+  };
+
   var td = $("#termDefinitions");
   if (!td.find("table").length) {
     function viewModel() {
@@ -223,9 +242,12 @@ require(["jquery", "underscore", "base/js/events", "knockout"], function($, _, e
       self.hightlightCell = function() {
         var cell = $("div.cell[data-cell-id='"+this.cell+"']");
         cell.addClass("alert alert-info");
+
+        $('#site').scrollTo(cell, { duration: 300, offsetTop: 200 });
+
         setTimeout(function() {
           cell.removeClass("alert alert-info")
-        }, 500);
+        }, 800);
       };
     };
     var model = new viewModel();
