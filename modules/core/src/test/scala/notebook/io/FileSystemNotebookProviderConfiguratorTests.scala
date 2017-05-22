75c27c13b27c0418145a6a92802e4245fc5d5b78
vidmantas zemleris <vidma@kensu.io>
2017-05-22 17:32:02 +0300

    Configure future wait time in FileSystemNotebookProviderConfiguratorTests

--- modules/core/src/test/scala/notebook/io/FileSystemNotebookProviderConfiguratorTests.scala
+++ modules/core/src/test/scala/notebook/io/FileSystemNotebookProviderConfiguratorTests.scala
@@ -7,10 +7,13 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import play.api.libs.json.{JsNumber, JsObject}
 import scala.collection.JavaConverters._
+import org.scalatest.time.{Millis, Seconds, Span}
 
 class FileSystemNotebookProviderConfiguratorTests extends WordSpec with Matchers with ScalaFutures with BeforeAndAfterAll {
 
   import scala.concurrent.ExecutionContext.Implicits.global
+
+  implicit val defaultPatience = PatienceConfig(timeout = Span(10, Seconds), interval = Span(500, Millis))
 
   var notebookDir : String = _
   var tempDir: Path = _
