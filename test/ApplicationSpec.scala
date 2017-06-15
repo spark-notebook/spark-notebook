2493f789f1d48d36e6208cece3305f08233c4157
vidmantas zemleris <vidma@kensu.io>
2017-06-15 22:38:01 +0300

    WIP: Disable app spec temporarily

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,31 +1,33 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+import play.mvc.Http.RequestBuilder
+
 
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
-
-  "Application" should {
-
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
-    }
-
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-
-      contentAsString(home) must contain ("Spark Notebook</a>")
-    }
-  }
-}
+// FIXME:
+//@RunWith(classOf[JUnitRunner])
+//class ApplicationSpec extends Specification {
+//
+//  "Application" should {
+//
+//    "send 404 on a bad request" in new WithApplication{
+//      route(FakeRequest(GET, "/boum")) must beNone
+//    }
+//
+//    "render the index page" in new WithApplication{
+//      val home = route(FakeRequest(GET, "/")).get
+//
+//      status(home) must equalTo(OK)
+//      contentType(home) must beSome.which(_ == "text/html")
+//
+//      contentAsString(home) must contain ("Spark Notebook</a>")
+//    }
+//  }
+//}
