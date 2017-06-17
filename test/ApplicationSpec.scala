906434771f1d2026f447bcaa75250d10aadeadf4
vidmantas zemleris <vidma@kensu.io>
2017-06-17 13:52:47 +0300

    Improve failing ApplicationSpec

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,33 +1,30 @@
-import org.specs2.mutable._
-import org.specs2.runner._
 import org.junit.runner._
+import org.specs2.runner._
+import play.api.mvc._
 import play.api.test._
-import play.api.test.Helpers._
-import play.mvc.Http.RequestBuilder
-
 
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-// FIXME:
+// FIXME: it now works with `test-only ApplicationSpec` but fails with `sbt test`
 //@RunWith(classOf[JUnitRunner])
-//class ApplicationSpec extends Specification {
+//class ApplicationSpec extends PlaySpecification with Results {
 //
 //  "Application" should {
+//      "send 404 on a bad request" in new WithApplication {
+//        val result = route(FakeRequest(GET, "/boum")).get
+//        status(result) must equalTo(NOT_FOUND)
+//      }
 //
-//    "send 404 on a bad request" in new WithApplication{
-//      route(FakeRequest(GET, "/boum")) must beNone
-//    }
+//      "render the index page" in new WithApplication {
+//        val home = route(FakeRequest(GET, "/")).get
 //
-//    "render the index page" in new WithApplication{
-//      val home = route(FakeRequest(GET, "/")).get
+//        status(home) must equalTo(OK)
+//        contentType(home) must beSome.which(_ == "text/html")
 //
-//      status(home) must equalTo(OK)
-//      contentType(home) must beSome.which(_ == "text/html")
-//
-//      contentAsString(home) must contain ("Spark Notebook</a>")
-//    }
+//        contentAsString(home) must contain("Spark Notebook</a>")
+//      }
 //  }
 //}
