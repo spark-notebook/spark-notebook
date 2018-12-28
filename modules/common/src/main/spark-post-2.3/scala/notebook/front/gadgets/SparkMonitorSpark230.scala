bad3b4c1cb07394f195da9e68caa09f91c427729
vidmantas zemleris <vidma@kensu.io>
2018-12-28 12:58:07 +0200

    Add job progress bar for spark 2.3+

--- modules/common/src/main/spark-post-2.3/scala/notebook/front/gadgets/SparkMonitorSpark230.scala
+++ modules/common/src/main/spark-post-2.3/scala/notebook/front/gadgets/SparkMonitorSpark230.scala
@@ -14,11 +14,9 @@ import play.api.libs.json._
 
 import notebook.util.Logging
 
-case class JobInfo(jobId: Int,
-                   completedTasks: Int,
-                   totalTasks: Int,
-                   submissionTime: Option[Long],
-                   completionTime: Option[Long])
+case class JobGroupInfo(jobGroup: String,
+                        completedTasks: Int,
+                        totalTasks: Int)
 
 // Dummy at the moment to make things work. no actual progress is reported
 class SparkMonitor(sparkContext:SparkContext, checkInterval:Long = 1000) extends Logging {
@@ -34,12 +32,50 @@ class SparkMonitor(sparkContext:SparkContext, checkInterval:Long = 1000) extends
 
   private[this] var t:Option[Thread] = None
 
+  def getJobGroupProgress(jobGroup: String) = {
+    val sc = sparkContext
+    // based on https://github.com/apache/zeppelin/pull/2750/files#diff-58542af0ab98166a4e47ff0af919cbd5R35
+    val jobIds = sc.statusTracker.getJobIdsForGroup(jobGroup)
+    val jobs = jobIds.flatMap { id => sc.statusTracker.getJobInfo(id) }
+    val stages = jobs.flatMap { job =>
+      job.stageIds().flatMap(sc.statusTracker.getStageInfo)
+    }
+
+    val taskCount = stages.map(_.numTasks).sum
+    val completedTaskCount = stages.map(_.numCompletedTasks).sum
+
+    JobGroupInfo(jobGroup = jobGroup, completedTasks = completedTaskCount, totalTasks = taskCount)
+  }
+
+  def fetchAllJobStatus: Seq[JsObject] = {
+    val allJobGroups = sparkContext.statusStore.jobsList(statuses = null).flatMap(_.jobGroup)
+    allJobGroups.map(getJobGroupProgress).map { j =>
+      val jobGroup = j.jobGroup
+      val cellId = JobTracking.toCellId(Some(jobGroup))
+      val jobDuration: Option[Long] = None
+      val jobDurationStr = "N/A"
+
+      Json.obj(
+        //"id" → j.jobId,
+        //"job" → j.jobId,
+        "group" → jobGroup,
+        "cell_id" → cellId,
+        "name" → jobGroup,
+        "completed" → (j.completedTasks.toDouble / j.totalTasks * 100),
+        "completed_tasks" -> j.completedTasks,
+        "total_tasks" -> j.totalTasks,
+        "duration_millis" → jobDuration,
+        "time" → jobDurationStr
+      )
+    }
+  }
+
   private[this] def newT ={
     new Thread(){
       override def run =
         while(true) {
           Thread.sleep(2000)
-          val m = Seq()
+          val m = fetchAllJobStatus
           connection <-- notebook.Connection.just(
             JsObject(Seq(
               "jobsStatus" -> JsArray(m),
